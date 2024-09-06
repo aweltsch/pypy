95c08cf671a66faaaf2714d66aba8460f94ad5e8
Andre Weltsch <andre@weltsch.de>
2024-09-06 08:39:19 +0200

    Resolve AttributeErrors in tests.

--- rpython/memory/gc/marksweep.py
+++ rpython/memory/gc/marksweep.py
@@ -1,4 +1,21 @@
 from rpython.memory.gc.base import GCBase
+from rpython.rtyper.lltypesystem import lltype
 
 class SimpleMarkSweepGC(GCBase):
-    pass
+    HDR = lltype.Struct('header', ('tid', lltype.Signed))
+
+    def malloc_fixedsize(self, typeid, size,
+                               needs_finalizer=False,
+                               is_finalizer_light=False,
+                               contains_weakptr=False):
+        pass
+
+    def malloc_varsize(self, typeid, length, size, itemsize,
+                             offset_to_length):
+        pass
+
+    def init_gc_object_immortal(self, addr, typeid16, flages=0):
+        pass
+
+    def collect(self):
+        pass
